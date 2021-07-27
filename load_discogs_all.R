d912800dae315a5b0796454c8ee5ad39839ccb5c
Mark Zwart <mark.zwart@pobox.com>
2021-07-28 00:11:30 +0200

    Add rudimentary artist loading flow

--- load_discogs_all.R
+++ load_discogs_all.R
@@ -42,21 +42,35 @@ if(config$load_collection){
   dbDisconnect(db_discogs)    
 }
 
+if(config$load_artists){
+  df_artists <- load_discogs_artists(df_collection_artists)
+  df_artist_images  <- extract_artist_images(df_artists)
+  df_artist_groups  <- extract_artist_groups(df_artists)
+  df_artist_aliases <- extract_artist_aliases(df_artists)
+  df_artist_members <- extract_artist_members(df_artists)
+  df_artist_urls    <- extract_artist_urls(df_artists)
+  df_artists        <- clean_artist_df(df_artists)
+  
+  # Write artist data to database
+  db_discogs <- dbConnect(RSQLite::SQLite(), paste0(config$db_location,"/discogs.sqlite"))
+  dbWriteTable(db_discogs, "artists",        df_artists,        overwrite = TRUE)
+  dbWriteTable(db_discogs, "artist_images",  df_artist_images,  overwrite = TRUE)
+  dbWriteTable(db_discogs, "artist_groups",  df_artist_groups,  overwrite = TRUE)
+  dbWriteTable(db_discogs, "artist_aliases", df_artist_aliases, overwrite = TRUE)
+  dbWriteTable(db_discogs, "artist_members", df_artist_members, overwrite = TRUE)
+  dbWriteTable(db_discogs, "artist_urls",    df_artist_urls,    overwrite = TRUE)
+  dbDisconnect(db_discogs)  
+} else {
+  
+  # Read artist data from database
+  db_discogs <- dbConnect(RSQLite::SQLite(), paste0(config$db_location,"/discogs.sqlite"))
+  df_artists        <- dbReadTable(db_discogs, "artists")
+  df_artist_images  <- dbReadTable(db_discogs, "artist_images")
+  df_artist_groups  <- dbReadTable(db_discogs, "artist_groups")
+  df_artist_aliases <- dbReadTable(db_discogs, "artist_aliases")
+  df_artist_members <- dbReadTable(db_discogs, "artist_members")
+  df_artist_urls    <- dbReadTable(db_discogs, "artist_urls")
+  dbDisconnect(db_discogs) 
+}
+  
 
-df_artists <- load_discogs_artists(df_collection_artists)
-df_artist_images  <- extract_artist_images(df_artists)
-df_artist_groups  <- extract_artist_groups(df_artists)
-df_artist_aliases <- extract_artist_aliases(df_artists)
-df_artist_members <- extract_artist_members(df_artists)
-df_artist_urls    <- extract_artist_urls(df_artists)
-df_artists        <- clean_artist_df(df_artists)
-
-# Write collection data to database
-db_discogs <- dbConnect(RSQLite::SQLite(), paste0(config$db_location,"/discogs.sqlite"))
-dbWriteTable(db_discogs, "artists",        df_artists,        overwrite = TRUE)
-dbWriteTable(db_discogs, "artist_images",  df_artist_images,  overwrite = TRUE)
-dbWriteTable(db_discogs, "artist_groups",  df_artist_groups,  overwrite = TRUE)
-dbWriteTable(db_discogs, "artist_aliases", df_artist_aliases, overwrite = TRUE)
-dbWriteTable(db_discogs, "artist_members", df_artist_members, overwrite = TRUE)
-dbWriteTable(db_discogs, "artist_urls",    df_artist_urls,    overwrite = TRUE)
-dbDisconnect(db_discogs)  
