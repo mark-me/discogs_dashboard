bda29f76712dee60ebcb49d47ffb1d1f27472329
Mark Zwart <mark.zwart@pobox.com>
2021-07-29 16:22:07 +0200

    Artist releases loading 1st attempt

--- load_discogs_all.R
+++ load_discogs_all.R
@@ -10,66 +10,52 @@ source("discogs_helper_functions.R")
 source("load_discogs_collection.R")
 source("load_discogs_artists.R")
 
-if(config$load_collection){
-  
-  df_collection_items   <- load_discogs_collection()
-  df_collection_artists <- extract_collection_artists(df_collection_items)
-  df_collection_labels  <- extract_collection_labels(df_collection_items)
-  df_collection_formats <- extract_collection_formats(df_collection_items)
-  df_collection_styles  <- extract_collection_styles(df_collection_items)
-  df_collection_genres  <- extract_collection_genres(df_collection_items)
-  df_collection_items   <- clean_collection_item_df(df_collection_items)
-  
-  # Write collection data to database
-  db_discogs <- dbConnect(RSQLite::SQLite(), paste0(config$db_location,"/discogs.sqlite"))
-  dbWriteTable(db_discogs, "collection_items",   df_collection_items,   overwrite = TRUE)
-  dbWriteTable(db_discogs, "collection_artists", df_collection_artists, overwrite = TRUE)
-  dbWriteTable(db_discogs, "collection_formats", df_collection_formats, overwrite = TRUE)
-  dbWriteTable(db_discogs, "collection_labels",  df_collection_labels,  overwrite = TRUE)
-  dbWriteTable(db_discogs, "collection_genres",  df_collection_genres,  overwrite = TRUE)
-  dbWriteTable(db_discogs, "collection_styles",  df_collection_styles,  overwrite = TRUE)
-  dbDisconnect(db_discogs)  
-} else {
-  
-  # Read collection data from database
-  db_discogs <- dbConnect(RSQLite::SQLite(), paste0(config$db_location,"/discogs.sqlite"))
-  df_collection_items   <- dbReadTable(db_discogs, "collection_items")
-  df_collection_artists <- dbReadTable(db_discogs, "collection_artists")
-  df_collection_formats <- dbReadTable(db_discogs, "collection_formats")
-  df_collection_labels  <- dbReadTable(db_discogs, "collection_labels")
-  df_collection_genres  <- dbReadTable(db_discogs, "collection_genres")
-  df_collection_styles  <- dbReadTable(db_discogs, "collection_styles")
-}
+df_collection_items   <- load_discogs_collection()
+df_collection_artists <- extract_collection_artists(df_collection_items)
+df_collection_labels  <- extract_collection_labels(df_collection_items)
+df_collection_formats <- extract_collection_formats(df_collection_items)
+df_collection_styles  <- extract_collection_styles(df_collection_items)
+df_collection_genres  <- extract_collection_genres(df_collection_items)
+df_collection_items   <- clean_collection_item_df(df_collection_items)
+
+# Write collection data to database
+db_discogs <- dbConnect(RSQLite::SQLite(), paste0(config$db_location,"/discogs.sqlite"))
+dbWriteTable(db_discogs, "collection_items",   df_collection_items,   overwrite = TRUE)
+dbWriteTable(db_discogs, "collection_artists", df_collection_artists, overwrite = TRUE)
+dbWriteTable(db_discogs, "collection_formats", df_collection_formats, overwrite = TRUE)
+dbWriteTable(db_discogs, "collection_labels",  df_collection_labels,  overwrite = TRUE)
+dbWriteTable(db_discogs, "collection_genres",  df_collection_genres,  overwrite = TRUE)
+dbWriteTable(db_discogs, "collection_styles",  df_collection_styles,  overwrite = TRUE)
 
 # Load artist data for new artists  
-df_artists        <- load_discogs_artists(db = db_discogs, df_collection_artists)
-if(nrow(df_artists) > 0){
-  df_artist_images  <- extract_artist_images(df_artists)
-  df_artist_groups  <- extract_artist_groups(df_artists)
-  df_artist_aliases <- extract_artist_aliases(df_artists)
-  df_artist_members <- extract_artist_members(df_artists)
-  df_artist_urls    <- extract_artist_urls(df_artists)
-  df_artists        <- clean_artist_df(df_artists)    
-  
-  # Write artist data to database
-  name_table <- "artists"
-  has_table <- dbExistsTable(db_discogs, name_table)
-  dbWriteTable(db_discogs, "artists",        df_artists,        overwrite = has_table)
-  dbWriteTable(db_discogs, "artist_images",  df_artist_images,  overwrite = has_table)
-  dbWriteTable(db_discogs, "artist_groups",  df_artist_groups,  overwrite = has_table)
-  dbWriteTable(db_discogs, "artist_aliases", df_artist_aliases, overwrite = has_table)
-  dbWriteTable(db_discogs, "artist_members", df_artist_members, overwrite = has_table)
-  dbWriteTable(db_discogs, "artist_urls",    df_artist_urls,    overwrite = has_table)
+if(config$reload_artist_discogs){
+
+    df_artists        <- load_discogs_artists(db = db_discogs, df_collection_artists)
+  if(nrow(df_artists) > 0){
+    df_artist_images  <- extract_artist_images(df_artists)
+    df_artist_groups  <- extract_artist_groups(df_artists)
+    df_artist_aliases <- extract_artist_aliases(df_artists)
+    df_artist_members <- extract_artist_members(df_artists)
+    df_artist_urls    <- extract_artist_urls(df_artists)
+    df_artists        <- clean_artist_df(df_artists)    
+    
+    # Write artist data to database
+    name_table <- "artists"
+    has_table <- dbExistsTable(db_discogs, name_table)
+    dbWriteTable(db_discogs, "artists",        df_artists,        overwrite = has_table)
+    dbWriteTable(db_discogs, "artist_images",  df_artist_images,  overwrite = has_table)
+    dbWriteTable(db_discogs, "artist_groups",  df_artist_groups,  overwrite = has_table)
+    dbWriteTable(db_discogs, "artist_aliases", df_artist_aliases, overwrite = has_table)
+    dbWriteTable(db_discogs, "artist_members", df_artist_members, overwrite = has_table)
+    dbWriteTable(db_discogs, "artist_urls",    df_artist_urls,    overwrite = has_table)
+  }
 }
 
 # Load data for new artist releases
-
-
-
+df_artists         <- dbReadTable(db_discogs, "artists")
+df_artist_releases <- load_discogs_artist_releases(db, df_artists)
 
 # Read artist data from database
-db_discogs <- dbConnect(RSQLite::SQLite(), paste0(config$db_location,"/discogs.sqlite"))
-df_artists        <- dbReadTable(db_discogs, "artists")
 df_artist_images  <- dbReadTable(db_discogs, "artist_images")
 df_artist_groups  <- dbReadTable(db_discogs, "artist_groups")
 df_artist_aliases <- dbReadTable(db_discogs, "artist_aliases")
--- load_discogs_artist_releases.R
+++ load_discogs_artist_releases.R
@@ -1,3 +1,54 @@
-load_discogs_artist_releases <- function(df_artists){
+load_discogs_artist_releases <- function(db, df_artists){
   
+  name_table <- "artist_releases"
+  
+  has_table <- dbExistsTable(db, name_table)
+  if(has_table){
+    df_previous <- dbReadTable(conn = db, name = name_table)
+    df_artists %<>% anti_join(df_previous, by = "id_artist")
+  }
+  
+  row <- 1
+  lst_releases <- list()
+  lst_json    <- list()
+  while(row <= nrow(df_artists)){
+    
+    lst_json <- api_request_artist_release(id_artist = df_artists[row, "id_artist"], 
+                                           idx_page = 1, 
+                                           api_discogs_config)
+    
+    for(idx_page in 1:lst_json$pagination$pages){
+      
+      lst_json <- api_request_artist_release(id_artist = df_artists[row, "id_artist"], 
+                                             idx_page = idx_page, 
+                                             api_discogs_config)     
+      
+      for(i in 1:length(lst_json$releases)){
+        lst_json$releases[[i]]$stats <- NULL
+        lst_json$releases[[i]]$id_artist <- df_artists[row, "id_artist"]
+      }
+      lst_releases <- c(lst_releases, lst_json$releases)
+    }
+  }  
+  
+  df_releases <- bind_rows(lst_releases)
+  return(df_releases)
+}
+
+api_request_artist_release <- function(id_artist, idx_page, api_discogs_config){
+ 
+  lst_json[[1]] <- "You are making requests too quickly."
+  while (lst_json[[1]] == "You are making requests too quickly.") {
+    
+    json <- GET(paste0(api_discogs_config$url, 
+                       "/artists/", id_artist, "/releases", 
+                       "?page=1&per_page=100",
+                       "&token=", api_discogs_config$token))
+    json_text <- content(json, as = "text", encoding = "UTF-8")
+    lst_json <- fromJSON(json_text)
+    if(lst_json[[1]] == "You are making requests too quickly."){
+      Sys.sleep(65)
+    }
+  }
+  return(lst_json)
 }
