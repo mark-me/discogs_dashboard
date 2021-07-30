00be8d5fa6a76fc236c7e0f3d62a25dc68a66625
Mark Zwart <mark.zwart@pobox.com>
2021-07-30 09:47:45 +0200

    First version loading releases

--- load_discogs_all.R
+++ load_discogs_all.R
@@ -28,38 +28,44 @@ dbWriteTable(db_discogs, "collection_genres",  df_collection_genres,  overwrite 
 dbWriteTable(db_discogs, "collection_styles",  df_collection_styles,  overwrite = TRUE)
 
 # Load artist data for new artists  
-if(config$reload_artist_discogs){
-
-    df_artists        <- load_discogs_artists(db = db_discogs, df_collection_artists)
-  if(nrow(df_artists) > 0){
-    df_artist_images  <- extract_artist_images(df_artists)
-    df_artist_groups  <- extract_artist_groups(df_artists)
-    df_artist_aliases <- extract_artist_aliases(df_artists)
-    df_artist_members <- extract_artist_members(df_artists)
-    df_artist_urls    <- extract_artist_urls(df_artists)
-    df_artists        <- clean_artist_df(df_artists)    
-    
-    # Write artist data to database
-    name_table <- "artists"
-    has_table <- dbExistsTable(db_discogs, name_table)
-    dbWriteTable(db_discogs, "artists",        df_artists,        overwrite = has_table)
-    dbWriteTable(db_discogs, "artist_images",  df_artist_images,  overwrite = has_table)
-    dbWriteTable(db_discogs, "artist_groups",  df_artist_groups,  overwrite = has_table)
-    dbWriteTable(db_discogs, "artist_aliases", df_artist_aliases, overwrite = has_table)
-    dbWriteTable(db_discogs, "artist_members", df_artist_members, overwrite = has_table)
-    dbWriteTable(db_discogs, "artist_urls",    df_artist_urls,    overwrite = has_table)
-  }
+df_artists        <- load_discogs_artists(db = db_discogs, df_collection_artists)
+if(nrow(df_artists) > 0){
+  df_artist_images  <- extract_artist_images(df_artists)
+  df_artist_groups  <- extract_artist_groups(df_artists)
+  df_artist_aliases <- extract_artist_aliases(df_artists)
+  df_artist_members <- extract_artist_members(df_artists)
+  df_artist_urls    <- extract_artist_urls(df_artists)
+  df_artists        <- clean_artist_df(df_artists)    
+  
+  # Write artist data to database
+  name_table <- "artists"
+  has_table <- dbExistsTable(db_discogs, name_table)
+  dbWriteTable(db_discogs, "artists",        df_artists,        overwrite = !has_table, append = has_table)
+  dbWriteTable(db_discogs, "artist_images",  df_artist_images,  overwrite = !has_table, append = has_table)
+  dbWriteTable(db_discogs, "artist_groups",  df_artist_groups,  overwrite = !has_table, append = has_table)
+  dbWriteTable(db_discogs, "artist_aliases", df_artist_aliases, overwrite = !has_table, append = has_table)
+  dbWriteTable(db_discogs, "artist_members", df_artist_members, overwrite = !has_table, append = has_table)
+  dbWriteTable(db_discogs, "artist_urls",    df_artist_urls,    overwrite = !has_table, append = has_table)
 }
 
 # Load data for new artist releases
-df_artists         <- dbReadTable(db_discogs, "artists")
-df_artist_releases <- load_discogs_artist_releases(db, df_artists)
+if(config$reload_artist_releases_discogs){
+  
+  df_artists         <- dbReadTable(db_discogs, "artists")
+  df_artist_releases <- load_discogs_artist_releases(db, df_artists)
+
+  # Write artist data to database
+  name_table <- "artist_releases"
+  has_table <- dbExistsTable(db_discogs, name_table)
+  dbWriteTable(db_discogs, name_table, df_artist_releases, overwrite = !has_table, append = has_table)
+} 
 
 # Read artist data from database
-df_artist_images  <- dbReadTable(db_discogs, "artist_images")
-df_artist_groups  <- dbReadTable(db_discogs, "artist_groups")
-df_artist_aliases <- dbReadTable(db_discogs, "artist_aliases")
-df_artist_members <- dbReadTable(db_discogs, "artist_members")
-df_artist_urls    <- dbReadTable(db_discogs, "artist_urls")
+df_artist_images   <- dbReadTable(db_discogs, "artist_images")
+df_artist_groups   <- dbReadTable(db_discogs, "artist_groups")
+df_artist_aliases  <- dbReadTable(db_discogs, "artist_aliases")
+df_artist_members  <- dbReadTable(db_discogs, "artist_members")
+df_artist_urls     <- dbReadTable(db_discogs, "artist_urls")
+df_artist_releases <- dbReadTable(db_discogs, "artist_releases")
 dbDisconnect(db_discogs) 
 
--- load_discogs_artist_releases.R
+++ load_discogs_artist_releases.R
@@ -8,15 +8,14 @@ load_discogs_artist_releases <- function(db, df_artists){
     df_artists %<>% anti_join(df_previous, by = "id_artist")
   }
   
-  row <- 1
-  lst_releases <- list()
-  lst_json    <- list()
-  while(row <= nrow(df_artists)){
+  lst_releases <- lst_json <- list()
+  for(row in 1:nrow(df_artists)){
     
     lst_json <- api_request_artist_release(id_artist = df_artists[row, "id_artist"], 
                                            idx_page = 1, 
                                            api_discogs_config)
-    
+    print(paste(row, "-", df_artists[row, "name_artist"]))
+    pb <- txtProgressBar(min = 0, max = lst_json$pagination$pages, style = 3)
     for(idx_page in 1:lst_json$pagination$pages){
       
       lst_json <- api_request_artist_release(id_artist = df_artists[row, "id_artist"], 
@@ -27,11 +26,13 @@ load_discogs_artist_releases <- function(db, df_artists){
         lst_json$releases[[i]]$stats <- NULL
         lst_json$releases[[i]]$id_artist <- df_artists[row, "id_artist"]
       }
+      setTxtProgressBar(pb, idx_page)
       lst_releases <- c(lst_releases, lst_json$releases)
     }
   }  
   
-  df_releases <- bind_rows(lst_releases)
+  df_releases <- bind_rows(lst_releases) %>% 
+    unique()
   return(df_releases)
 }
 
@@ -42,7 +43,7 @@ api_request_artist_release <- function(id_artist, idx_page, api_discogs_config){
     
     json <- GET(paste0(api_discogs_config$url, 
                        "/artists/", id_artist, "/releases", 
-                       "?page=1&per_page=100",
+                       "?page=", idx_page, "&per_page=100",
                        "&token=", api_discogs_config$token))
     json_text <- content(json, as = "text", encoding = "UTF-8")
     lst_json <- fromJSON(json_text)
