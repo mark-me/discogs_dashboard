c0e9c4c43bb3afede4d2e256cd5f3fe85091122b
Mark Zwart <mark.zwart@pobox.com>
2021-08-23 11:43:13 +0200

    Clean code

--- scraps/navigate_through_clusters.R
+++ scraps/navigate_through_clusters.R
@@ -14,44 +14,6 @@ config <- read_yaml("config.yml")
 graph_releases <- get_artist_clusters(FALSE)
 clust_releases <- read_rds("cluster_edge_betweenness.rds")
 
-get_cluster <- function(graph_releases, clust_releases, name_node_selected = NA){
-  
-  # Cluster until the number of clusters in the sub graph
-  qty_clusters_min <- 6
-  qty_clusters <- 0
-  no_communities <- qty_clusters_min
-  while(qty_clusters < qty_clusters_min){
-    
-    no_communities <- no_communities + 1
-    id_communities <- cut_at(clust_releases, no = no_communities)
-    V(graph_releases)$cluster <- id_communities
-    
-    if(!is.na(name_node_selected)){
-      graph_clusters <- make_ego_graph(graph_releases, 
-                                       order = length(V(graph_releases)),
-                                       nodes = V(graph_releases)[name_node_selected])[[1]]
-    } else {
-      graph_clusters <- graph_releases 
-    }
-    
-    qty_clusters <- length(unique(V(graph_clusters)$cluster))
-  }
-  
-  # Aggregate node data
-  df_agg_node_attr <- aggregate_node_attributes(graph_clusters, qty_authoritative = 3)
-  V(graph_clusters)[df_agg_node_attr$name]$name_performer <- df_agg_node_attr$name_performer_clust
-  V(graph_clusters)[df_agg_node_attr$name]$qty_nodes      <- df_agg_node_attr$qty_nodes_clust
-  V(graph_clusters)[df_agg_node_attr$name]$qty_collection <- df_agg_node_attr$qty_collection_clust
-  V(graph_clusters)[df_agg_node_attr$name]$qty_releases   <- df_agg_node_attr$qty_releases_clust
-  
-  # Create a aggregated graph
-  graph_contracted <- contract(graph_clusters, V(graph_clusters)$cluster,
-                               vertex.attr.comb = list("first"))
-  graph_contracted <- simplify(graph_contracted)
-  
-  return(graph_contracted)
-}
-
 aggregate_node_attributes <- function(graph_clusters, qty_authoritative){
   
   df_nodes <- as_data_frame(graph_clusters, what = "vertices")
@@ -81,38 +43,9 @@ aggregate_node_attributes <- function(graph_clusters, qty_authoritative){
   return(df_nodes)
 }
 
-# # Make a subcluster of the chosen cluster one level lower than previous
-# graph_selected <- get_cluster(graph_releases, clust_releases)
-# V(graph_selected)$label <- paste0(V(graph_selected)$cluster, " - ", 
-#                                   V(graph_selected)$qty_nodes, "\n",
-#                                   V(graph_selected)$name_performer
-#                                   )
-# plot(graph_selected)
-# 
-# name_node_selected <- "p_1003228"
-# V(graph_selected)[name_node_selected]$name_performer
-# V(graph_selected)[name_node_selected]$cluster
-# graph_selected <- get_cluster(graph_releases, clust_releases, name_node_selected)
-# V(graph_selected)$label <- paste0(V(graph_selected)$cluster, " - ", 
-#                                   V(graph_selected)$qty_nodes, "\n",
-#                                   V(graph_selected)$name_performer
-# )
-# plot(graph_selected)
-# 
-# name_node_selected <- "p_2184902"
-# V(graph_selected)[name_node_selected]$name_performer
-# V(graph_selected)$label <- paste0(V(graph_selected)$name, " - ", 
-#                                   V(graph_selected)$qty_nodes, "\n",
-#                                   V(graph_selected)$name_performer
-# )
-# V(graph_selected)[name_node_selected]$name_performer
-# plot(graph_selected)
-
-# Gets stuck at a level
-
-# Second go
 get_next_iter <- function(res_clust, search_item, search_item_previous = NA){
-  
+
+  # Iterate through network from top down   
   if(is.na(search_item_previous)){
     
     qty_steps <- length(res_clust$merges)/2
@@ -142,8 +75,10 @@ get_next_iter <- function(res_clust, search_item, search_item_previous = NA){
   return(search_item)
 }
 
+
 aggregate_network <- function(graph_clusters){
   
+  # Aggregate release network to the clusters 
   df_agg_node_attr <- aggregate_node_attributes(graph_clusters, qty_authoritative = 3)
   V(graph_clusters)[df_agg_node_attr$name]$name_performer <- df_agg_node_attr$name_performer_clust
   V(graph_clusters)[df_agg_node_attr$name]$qty_nodes      <- df_agg_node_attr$qty_nodes_clust
@@ -161,6 +96,8 @@ aggregate_network <- function(graph_clusters){
   return(graph_contracted)
 }
 
+
+# Create test search path
 lst_search_item <- list()
 lst_search_item[[1]] <- list()
 
@@ -171,7 +108,6 @@ search_item <- lst_search_item[[1]]
 graph_clusters <- graph_releases 
 V(graph_clusters)$cluster <- search_item$id_communities
 V(graph_clusters)$is_selected <- search_item$is_cluster_selected
-#graph_clusters <- delete_vertices(graph_clusters, V(graph_clusters)[!search_item$is_cluster_selected])
 graph_aggr <- aggregate_network(graph_clusters)
 V(graph_aggr)$label <- paste0(V(graph_aggr)$cluster, " - ", 
                               V(graph_aggr)$qty_nodes, "\n",
@@ -211,8 +147,3 @@ V(graph_aggr)$label <- paste0(V(graph_aggr)$cluster, " - ",
                               V(graph_aggr)$name_performer
 )
 plot(graph_aggr)
-
-
-with(search_item, table(id_communities[is_cluster_selected]))
-table(V(graph_aggr)$cluster, useNA = "ifany")
-
