000b51e7e48ac0c74480b716a54c3e07d4775c94
Mark Zwart <mark.zwart@pobox.com>
2021-11-27 01:59:33 +0100

    Adding treemap

--- scraps/drawTreemap.R
+++ scraps/drawTreemap.R
@@ -43,3 +43,105 @@ tm <- voronoiTreemap(
 drawTreemap(tm, 
             color_type = "categorical", color_level = 1,
             label_size = rep(3,nrow(df_nodes)))
+
+
+library(tidyverse)
+library(magrittr)
+library(RSQLite)
+library(yaml)
+library(scales)
+library(ggimage)
+library(visNetwork)
+source("discogs_dashboard/get_network_data_functions.R")
+source("discogs_dashboard/cluster_navigation.R")
+
+config <- read_yaml("config.yml")
+file_db <- paste0(config$db_location, "/discogs.sqlite")
+
+lst_network <- list(
+  res_clustering = read_rds(config$file_cluster_results),
+  nw_performer_releases = get_performer_master_network(file_db)
+)
+
+
+get_network <- function(lst_network, lst_search_results = NA, id_cluster_selected = NA){
+  
+  nw <- lst_network$nw_performer_releases
+  
+  if(is.na(lst_search_results)){
+    # Get least number of clusters if no previous search results are provided
+    lst_search_result <- get_clusters(lst_network$res_clustering)  
+  } else {
+    # Get clustering based on the previous aggregation level and a chosen node within that aggregation
+    lst_search_result <- get_clusters(res_clustering = lst_network$res_clustering,
+                                      id_step = lst_search_results[[length(lst_search_results)]]$id_step, 
+                                      id_cluster_selected = id_cluster_selected) 
+  }
+  
+  nw$df_nodes %<>% left_join(lst_search_result$df_cluster, by = "id_node") # Add cluster ID's to nodes
+  
+  return(nw)
+}
+
+
+lst_search_results <- list()
+
+i <- 1
+lst_search_results[[i]] <- get_network(lst_network, lst_search_results = NA, id_cluster_selected = NA)
+
+
+df_nodes <- lst_search_results[[1]]$df_nodes %>%
+  filter(type_node == "performer") %>% 
+  filter(qty_collection_items > 0) %>% 
+  mutate(qty_collection_log = log(qty_collection_items) + 1) 
+
+tm <- voronoiTreemap(
+  data = df_nodes,
+  levels = c("id_cluster", "name_node"),
+  cell_size = "qty_collection_log",
+  shape = "rounded_rect"
+)
+
+drawTreemap(tm, 
+            color_type = "categorical", color_level = 1,
+            label_size = df_nodes$qty_collection_log*10)
+
+i <- 2
+lst_search_results[[i]] <- get_network(lst_network, lst_search_results, id_cluster_selected = 1)
+
+df_nodes <- lst_search_results[[i]]$nw_cluster$df_nodes
+
+df_nodes %<>%
+  filter(qty_collection > 0) %>% 
+  mutate(qty_collection_log = log(qty_collection) + 1)
+
+tm <- voronoiTreemap(
+  data = df_nodes,
+  levels = c("id_cluster", "label"),
+  cell_size = "qty_collection_log",
+  shape = "rounded_rect"
+)
+
+drawTreemap(tm, 
+            color_type = "categorical", color_level = 1,
+            label_size = rep(3,nrow(df_nodes)))
+
+i <- 3
+lst_search_results[[i]] <- get_clustered_network(lst_network, lst_search_results, id_cluster_selected = 127)
+
+df_nodes <- lst_search_results[[i]]$nw_cluster$df_nodes
+
+df_nodes %<>%
+  filter(qty_collection > 0) %>% 
+  mutate(qty_collection_log = log(qty_collection) + 1)
+
+tm <- voronoiTreemap(
+  data = df_nodes,
+  levels = c("id_cluster", "label"),
+  cell_size = "qty_collection_log",
+  shape = "rounded_rect"
+)
+
+drawTreemap(tm, 
+            color_type = "categorical", color_level = 1,
+            label_size = rep(3,nrow(df_nodes)))
