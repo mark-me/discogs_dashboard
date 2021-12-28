5d0355669d1c44b25d30455f92925a0da07e7dfb
Mark Zwart <mark.zwart@pobox.com>
2021-12-28 23:46:04 +0100

    Oops 2

--- discogs_dashboard/test_prep_down_drilling.R
+++ discogs_dashboard/test_prep_down_drilling.R
@@ -20,4 +20,17 @@ lst_search_results <- list()
 
 i <- 1
 lst_search_results[[i]] <- get_clustered_network(lst_network, lst_search_results = NA, id_cluster_selected = NA)
+# lst_search_results[[i]]$nw_cluster$df_nodes %<>%
+#   mutate(id_cluster = paste0(lst_search_results[[i]]$id_step_hierarchy, "_", id_cluster)) 
+
 plot_network(lst_search_results[[i]]$nw_cluster)
+
+id_clusters <- unique(lst_search_results[[i]]$nw_  $id_cluster)
+i <- 2
+for(id_cluster in id_clusters){
+  
+  lst_search_results[[i]] <- get_clustered_network(lst_network, lst_search_results, id_cluster_selected = id_cluster)
+  i <- i + 1
+}
+plot_network(lst_search_results[[i-11]]$nw_cluster)
+
